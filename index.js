ceeadc8c31af8b85189630d1a5422cbde2773245
Abks-Codes <shresthaabks@gmail.com>
2025-11-27 22:06:52 -0800

    starting on javascript

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+let backgroundVideo = document.getElementById("video");
+let displayProfile = document.getElementById("profile");
+let displayName = document.getElementById("displayName");
+*/
+function generate(event)
+{
+event.preventDefault();
+
+
+    const username = document.getElementById("username").value;
+    const background = document.getElementById("customBackground").value;
+    const profilePic = document.getElementById("Profile").value;
+    const name = document.getElementById("name").value;
+    const quote = document.getElementById("quote").value;
+    const discordLink = document.getElementById("discordLink").value;
+    const spotifyLink = document.getElementById("spotifyLink").value;
+    const twitchLink = document.getElementById("twitchLink").value;
+
+    /*  localStorage.setItem("username", username);
+   localStorage.setItem("background", background);
+  localStorage.setItem("name", name);
+    localStorage.setItem("quote", quote);
+    localStorage.setItem("discordLink", discordLink);
+    localStorage.setItem("spotifyLink", spotifyLink);
+    localStorage.setItem("twitchLink", twitchLink);
+
+    displayName.textContent = `${username}`;
+    window.location.href = "index.html";
+ */
+
+}
